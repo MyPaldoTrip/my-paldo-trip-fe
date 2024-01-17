94b409758741cf6ae558ba0b31c13f8593d28496
asdvsvs <gag973000@gmail.com>
2024-01-18 03:59:53 +0900

    [#18] feat : 카카오 로그인 추가

--- src/views/user/loginUser.vue
+++ src/views/user/loginUser.vue
@@ -12,6 +12,17 @@
       <button type="submit">Submit</button>
     </form>
   </div>
+
+  <div class="wrap">
+    <div class="title">로그인</div>
+    <a class="kakao"
+       href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e7d865af6b6955f7a10c13318366d5c9&redirect_uri=http://localhost:8080/kakao-login/oauth2/code">
+
+      <div class="kakao_i"></div>
+      <div class="kakao_txt">카카오톡으로 간편로그인</div>
+    </a>
+
+  </div>
 </template>
 
 <script>
@@ -85,4 +96,56 @@ export default {
 .login-form button:hover {
   background-color: #0056b3;
 }
+
+.wrap {
+  width: 490px;
+  padding: 40px 20px 20px 20px;
+  background-color: #f5f6f7;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 30px;
+  box-sizing: border-box;
+}
+
+.title {
+  margin: 0 auto;
+  width: 240px;
+  height: 44px;
+  text-align: center;
+  font-size: 25px;
+  background-repeat: no-repeat;
+  background-position: 0 0;
+  background-size: 240px auto;
+  margin-bottom: 20px;
+}
+
+.kakao {
+  margin-top: 15px;
+  height: 60px;
+  border: solid 1px #FEE500;
+  background: #FEE500;
+  color: #3c1d1e;
+  font-size: 18px;
+  box-sizing: border-box;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 450px;
+  display: flex;
+}
+
+.kakao_txt {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  padding-right: 60px;
+}
+
+a {
+  text-decoration: none;
+}
+
 </style>
