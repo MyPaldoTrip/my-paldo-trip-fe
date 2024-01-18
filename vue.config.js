83ffea4dea52064cf15acfa8cce931bbef93917e
asdvsvs <gag973000@gmail.com>
2024-01-19 03:27:40 +0900

    feat : 요청 주소 설정 파일에서 관리?하도록

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        // target: 'http://43.200.254.60:8080' //ec2 퍼블릭 주소, 배포용
+        target: 'http://localhost:8080' //로컬호스트 기본 주소, 개발용
+      }
+    }
+  }
 })
