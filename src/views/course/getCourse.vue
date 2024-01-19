8827e710eb2b266bd6c4496337fc7a94ae8629c8
Sk1palong <cchs33@gmail.com>
2024-01-19 16:17:43 +0900

    [#13] feat : alert 추가, 호출 api url변경, 작성자 추가

--- src/views/course/getCourse.vue
+++ src/views/course/getCourse.vue
@@ -2,7 +2,7 @@
   <div v-if="course">
     <h2>{{ course.title }}</h2>
     <p>코스 번호: {{ course.courseId }}</p>
-    <p>작성자: {{ course.author }}</p>
+    <p>작성자: {{ course.username }}</p>
     <p>내용: {{ course.content }}</p>
     <div v-for="(url, index) in course.fileURL" :key="index">
       <p>파일 URL: <a :href="url" target="_blank">{{ url }}</a></p>
@@ -23,13 +23,14 @@ export default {
   methods: {
     fetchCourse() {
       const courseId = this.$route.params.courseId;
-      axios.get(`http://localhost:8080/api/v1/courses/${courseId}`)
+      axios.get(`/api/v1/courses/${courseId}`)
       .then(response => {
         console.log(response.data)
         this.course = response.data.data;
       })
       .catch(error => {
         console.error('Error:', error);
+        alert('error')
       });
     }
   },
