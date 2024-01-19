a55ee5492bdb525d3bbea7963b69a419ac85625f
Sk1palong <cchs33@gmail.com>
2024-01-19 16:26:24 +0900

    [#27] feat : 초기 좋아요 상태 체크 추가, Auth값 localStorage에서 가져옴, 호출 api url변경, 좋아요 토글 정상작동

--- src/views/like/toggleLike.vue
+++ src/views/like/toggleLike.vue
@@ -1,39 +1,56 @@
 <template>
   <div>
 
-    <button @click="toggleLike">
-      <span v-if="likeStatus==='좋아요'">♥x</span>
-      <span v-else>♥</span>
-    </button>
+    <button @click="toggleLike"></button>
+    <span v-if="likeStatus.isLiked">♡</span>
+    <span v-else>♥️</span>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import { useRoute } from 'vue-router';
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 export default {
   props: ['courseId'],
   setup() {
     const route = useRoute();
     const courseId = route.params.courseId;
-    const likeStatus = ref('좋아요')
+    const likeStatus = ref(false)
+
+    const likeCheck = () => {
+      axios.get(`http://localhost:8080/api/v1/courses/${courseId}/likes`, {
+        headers: {
+          Authorization: localStorage.getItem('Authorization')
+        }
+      })
+      .then(response => {
+        console.log('res',response)
+        likeStatus.value = response.data.data;
+        console.log('zz',response.data.data.isLiked);
+      })
+      .catch(error => {
+        console.error('Error',error)
+      });
+    };
+
     const toggleLike = () => {
       axios.post(`http://localhost:8080/api/v1/courses/${courseId}/likes`, {}, {
         headers: {
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhQGVtYWlsLmNvbSIsImV4cCI6MTcwNTU2NjQwNSwiaWF0IjoxNzA1NTYyODA1fQ.nuCBybqXSz_WWl2uF7vebHvmHdKXLYOgGATsRBQqNHA' // 실제 토큰 값으로 대체해야 합니다.
+          Authorization: localStorage.getItem('Authorization')
         }
       })
       .then(response => {
         console.log('Like Toggled:', response.data.data);
-        likeStatus.value = response.data.data.msg;
-        console.log(likeStatus)
+        console.log('likeStatus',likeStatus)
+        likeCheck();
       })
       .catch(error => {
         console.error('Error:', error);
       });
     };
 
+    onMounted(() => likeCheck());
     return {
       likeStatus,
       toggleLike
