6b4037a62494d74c21b2ff6765e1368e6f740ce3
jimyung0418 <wjdwlaud0418@naver.com>
2024-01-21 03:03:48 +0900

    [#41] feat : 여행정보 파일 단건 조회 페이지에 파일 삭제 기능 추가

--- src/views/trip/getTripFile.vue
+++ src/views/trip/getTripFile.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="center">
     <h2>파일 상세 정보</h2>
     <img :src="file.fileUrl" alt="Selected file">
+    <button @click="deleteFile">파일 삭제</button>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import { onMounted, ref } from "vue";
 import axios from "axios";
 import { useRoute } from "vue-router";
+import router from "@/router";
 
 const route = useRoute();
 const tripId = route.params.tripId;
@@ -31,10 +33,37 @@ const getTripFile = async () => {
 };
 onMounted(getTripFile);
 
+const deleteFile = async () => {
+  if (!confirm('정말 삭제하시겠습니까?')) {
+    return;
+  }
 
+  try {
+    await axios.delete(`http://localhost:8080/api/v1/trips/${tripId}/files/${fileId}`, {
+      headers: {
+        'Authorization': localStorage.getItem('Authorization'),
+      },
+    });
+
+    alert('파일이 삭제되었습니다.');
+    router.push(`/getTrip/${tripId}/fileList`).catch(error => {
+      console.error('There was an error routing to the file list:', error);
+    });
+  } catch (error) {
+    console.error('There was an error deleting the file:', error);
+  }
+};
 </script>
 
 
 <style scoped>
+.center {
+  text-align: center;
+}
+
+button {
+  display: block;
+  margin: 20px auto;
+}
 
 </style>
