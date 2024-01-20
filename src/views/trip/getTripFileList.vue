7382e41f9cafc0ae7fe53783cafcd6fb42b65a0d
jimyung0418 <wjdwlaud0418@naver.com>
2024-01-21 03:29:11 +0900

    [#38] feat : 여행정보 파일 목록 조회 페이지에 파일 업로드 기능 추가

--- src/views/trip/getTripFileList.vue
+++ src/views/trip/getTripFileList.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="trip-file-list">
     <h2>파일 목록</h2>
+    <div>
+      <label for="file-upload" class="custom-file-upload">
+        파일 추가
+      </label>
+      <input id="file-upload" type="file" @change="uploadFile" style="display: none;">
+    </div>
     <ul class="image-list">
       <li v-for="file in tripFileList" :key="file.tripFileId" class="image-item">
         <router-link :to="`/getTrip/${tripId}/files/${file.tripFileId}`">
@@ -19,6 +25,39 @@ import {useRoute} from "vue-router";
 const route = useRoute();
 const tripId = route.params.id;
 const tripFileList = reactive([]);
+
+const uploadFile = async (event) => {
+  const files = event.target.files;
+
+  if (!files || files.length === 0) {
+    return;
+  }
+
+  const file = files[0];
+
+  if (!window.confirm(`'${file.name}' 파일을 업로드하시겠습니까?`)) {
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('multipartFile', file);
+
+
+  try {
+    const response = await axios.post(`http://localhost:8080/api/v1/trips/${tripId}/files`, formData, {
+      headers: {
+        'Authorization': localStorage.getItem('Authorization'),
+        'Content-Type': 'multipart/form-data'
+      },
+    });
+
+    alert('파일이 업로드되었습니다.');
+    tripFileList.push(response.data.data);
+  } catch (error) {
+    console.error('There was an error uploading the file:', error);
+  }
+};
+
 
 const getTripFileList = async () => {
   try {
@@ -39,6 +78,20 @@ onMounted(getTripFileList);
 
 
 <style scoped>
+.custom-file-upload {
+  padding: 10px;
+  cursor: pointer;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  text-align: center;
+}
+
+.custom-file-upload:hover {
+  background-color: #45a049;
+}
+
 .image-list {
   display: flex;
   justify-content: space-around;
