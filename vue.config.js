3110d0c850ba5d79f3869fb166966cbc2117c6ad
asdvsvs <gag973000@gmail.com>
2024-01-21 17:54:16 +0900

    ec2 주소 변경

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://43.200.254.60:8080' //ec2 퍼블릭 주소, 배포용
+        target: 'http://43.201.57.160:8080' //ec2 퍼블릭 주소, 배포용
         // target: 'http://localhost:8080' //로컬호스트 기본 주소, 개발용
       }
     }
