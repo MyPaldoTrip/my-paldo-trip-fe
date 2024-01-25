848477cb8d39f53548d10d50af967fffbd3cb5c3
asdvsvs <gag973000@gmail.com>
2024-01-26 01:39:10 +0900

    fix : 홈 화면 수정

--- src/assemble/HomePage.vue
+++ src/assemble/HomePage.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
     <vueHome/>
-    <vueInputDataTest/>
-    <!--    <vueDataTest/>-->
   </div>
 </template>
 
 <script>
-// import vueDataTest from "@/views/sample/VueDataTest.vue";
 import vueHome from "@/views/sample/VueHome.vue";
-import vueInputDataTest from "@/views/sample/VueInputDataTest.vue";
 
 export default {
   components: {
     vueHome,
-    vueInputDataTest,
-    // vueDataTest
   }
 }
 </script>
